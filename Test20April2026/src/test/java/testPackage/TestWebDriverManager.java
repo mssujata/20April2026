ec2542438e471c1b93faf7b735824c4d79f0bc92
HP <HP@DESKTOP-5FGHJLR>
2026-07-29 22:42:56 -0400

    Adding 20 April 2026 Project

---
+++ Test20April2026/src/test/java/testPackage/TestWebDriverManager.java
@@ -0,0 +1,19 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestWebDriverManager {
+
+	public static void main(String[] args) throws Exception {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		Thread.sleep(10000);
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+	}
+
+}
